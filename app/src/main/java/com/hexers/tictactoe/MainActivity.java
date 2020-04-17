f33fb72b77e99d6c40dc71c7614bbe90ac612ac4
Hexers <hexijasqx@gmail.com>
2020-04-16 19:26:14 -0500

    Updates to basic tic-tac-toe layout

--- app/src/main/java/com/hexers/tictactoe/MainActivity.java
+++ app/src/main/java/com/hexers/tictactoe/MainActivity.java
@@ -1,14 +1,273 @@
 package com.hexers.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.text.NumberFormat;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity
+        implements OnEditorActionListener, OnClickListener {
+
+    // define variables for the widgets
+    /*
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button   percentUpButton;
+    private Button   percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private TextView nameTextView;
+    */
+
+    private TextView namePlayerOneTextView;
+    private TextView namePlayerTwoTextView;
+    private Button tileOneButton;
+    private Button tileTwoButton;
+    private Button tileThreeButton;
+    private Button tileFourButton;
+    private Button tileFiveButton;
+    private Button tileSixButton;
+    private Button tileSevenButton;
+    private Button tileEightButton;
+    private Button tileNineButton;
+
+    // define instance variables that should be saved
+    private String billAmountString = "";
+    private float tipPercent = .15f;
+
+    // define rounding constants
+    private final int ROUND_NONE = 0;
+    private final int ROUND_TIP = 1;
+    private final int ROUND_TOTAL = 2;
+
+    // set up preferences
+    private SharedPreferences prefs;
+    private boolean rememberTipPercent = true;
+    private int rounding = ROUND_NONE;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get references to the widgets
+
+        namePlayerOneTextView = (TextView) findViewById(R.id.namePlayerOneTextView);
+        namePlayerTwoTextView = (TextView) findViewById(R.id.namePlayerTwoTextView);
+        tileOneButton = (Button) findViewById(R.id.tileOneButton);
+        tileTwoButton = (Button) findViewById(R.id.tileTwoButton);
+        tileThreeButton = (Button) findViewById(R.id.tileThreeButton);
+        tileFourButton = (Button) findViewById(R.id.tileFourButton);
+        tileFiveButton = (Button) findViewById(R.id.tileFiveButton);
+        tileSixButton = (Button) findViewById(R.id.tileSixButton);
+        tileSevenButton = (Button) findViewById(R.id.tileSevenButton);
+        tileEightButton = (Button) findViewById(R.id.tileEightButton);
+        tileNineButton = (Button) findViewById(R.id.tileNineButton);
+
+        // set the listeners
+        tileOneButton.setOnClickListener(this);
+        tileTwoButton.setOnClickListener(this);
+        tileThreeButton.setOnClickListener(this);
+        tileFourButton.setOnClickListener(this);
+        tileFiveButton.setOnClickListener(this);
+        tileSixButton.setOnClickListener(this);
+        tileSevenButton.setOnClickListener(this);
+        tileEightButton.setOnClickListener(this);
+        tileNineButton.setOnClickListener(this);
+
+
+        // set the default values for the preferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        // get default SharedPreferences object
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        Editor editor = prefs.edit();
+        editor.putString("billAmountString", billAmountString);
+        editor.putFloat("tipPercent", tipPercent);
+        editor.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get preferences
+        rememberTipPercent = prefs.getBoolean("pref_remember_percent", true);
+        rounding = Integer.parseInt(prefs.getString("pref_rounding", "0"));
+
+        // get name for player one
+        String playerOneName = prefs.getString("player_one_name", "");
+        namePlayerOneTextView.setText(playerOneName);
+
+        // get name for player two
+        String playerTwoName = prefs.getString("player_two_name", "");
+        namePlayerTwoTextView.setText(playerTwoName);
+
+        // get the instance variables
+        billAmountString = prefs.getString("billAmountString", "");
+        if (rememberTipPercent) {
+            tipPercent = prefs.getFloat("tipPercent", 0.15f);
+        } else {
+            tipPercent = 0.15f;
+        }
+
+        // set the bill amount on its widget
+        // billAmountEditText.setText(billAmountString);
+
+        // calculate and display
+        calculateAndDisplay();
+    }
+
+    public void calculateAndDisplay() {
+        // get the bill amount
+        // billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+
+        // calculate tip and total
+        float tipAmount = 0;
+        float totalAmount = 0;
+        float tipPercentToDisplay = 0;
+        if (rounding == ROUND_NONE) {
+            tipAmount = billAmount * tipPercent;
+            totalAmount = billAmount + tipAmount;
+            tipPercentToDisplay = tipPercent;
+        }
+        else if (rounding == ROUND_TIP) {
+            tipAmount = StrictMath.round(billAmount * tipPercent);
+            totalAmount = billAmount + tipAmount;
+            tipPercentToDisplay = tipAmount / billAmount;
+        }
+        else if (rounding == ROUND_TOTAL) {
+            float tipNotRounded = billAmount * tipPercent;
+            totalAmount = StrictMath.round(billAmount + tipNotRounded);
+            tipAmount = totalAmount - billAmount;
+            tipPercentToDisplay = tipAmount / billAmount;
+        }
+
+        // display the other results with formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        //tipTextView.setText(currency.format(tipAmount));
+        //totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        //percentTextView.setText(percent.format(tipPercentToDisplay));
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        int keyCode = -1;
+        if (event != null) {
+            keyCode = event.getKeyCode();
+        }
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED ||
+                keyCode == KeyEvent.KEYCODE_DPAD_CENTER ||
+                keyCode == KeyEvent.KEYCODE_ENTER) {
+            calculateAndDisplay();
+        }
+        return false;
+    }
+
+    /*
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tileOneButton:
+
+                break;
+            case R.id.tileTwoButton:
+
+                break;
+            case R.id.tileThreeButton:
+
+                break;
+            case R.id.tileFourButton:
+
+                break;
+            case R.id.tileFiveButton:
+
+                break;
+            case R.id.tileSixButton:
+
+                break;
+            case R.id.tileSevenButton:
+
+                break;
+            case R.id.tileEightButton:
+
+                break;
+            case R.id.tileNineButton:
+
+                break;
+            case R.id.newGameButton:
+
+                break;
+        }
+    }
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tileOneButton:
+                tipPercent=tipPercent-.01f;
+                calculateAndDisplay();
+            break;
+            case R.id.tileTwoButton:
+                tipPercent=tipPercent+.01f;
+                calculateAndDisplay();
+            break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                // Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                return true;
+            case R.id.menu_about:
+                // Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
